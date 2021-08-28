04687203f8d287caad19a65fefd11224481f81aa
Erdal Karaca <erdal.karaca.de@gmail.com>
2021-08-28 09:56:55 +0200

    gh-11: provide list of required packages to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys
+import sys, os
 from setuptools import setup
 import versioneer
 
@@ -9,9 +9,14 @@ import versioneer
 SETUP_REQUIRES = ['setuptools >= 30.3.0']
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
+INSTALL_REQUIRES = []
 
 if __name__ == '__main__':
+    REQ_FILE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/requirements.txt"
+    with open(REQ_FILE_PATH) as f:
+        INSTALL_REQUIRES = list(f.read().splitlines())
     setup(name="ancpbids",
           version=versioneer.get_version(),
           cmdclass=versioneer.get_cmdclass(),
-          setup_requires=SETUP_REQUIRES)
+          setup_requires=SETUP_REQUIRES,
+          install_requires=INSTALL_REQUIRES)
