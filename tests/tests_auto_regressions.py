7c5108caf8ac6d812184ab729c00acca6ca36a18
Erdal Karaca <erdal.karaca.de@gmail.com>
2022-02-22 23:18:50 +0100

    use own query language and get rid of lxml dependency

--- tests/tests_auto_regressions.py
+++ tests/tests_auto_regressions.py
@@ -6,7 +6,8 @@ class RegressionsTestCase(BaseTestCase):
     def test_get_all_files_from_pipeline(self):
         layout = ancpbids.BIDSLayout(DS005_DIR)
         all_derivative_files = layout.get(scope='derivatives/affine/matrix', return_type='file')
-        self.assertEqual(16, len(all_derivative_files))
+        # that derivatives folder has no valid BIDS files
+        self.assertEqual(0, len(all_derivative_files))
 
 
 if __name__ == '__main__':
