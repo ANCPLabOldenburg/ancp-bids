c22987e296e1b0f533537262a306a75409c195d0
Erdal Karaca <erdal.karaca.de@gmail.com>
2022-02-22 23:23:39 +0100

    add regression test for layout.get_metadata() without params

--- tests/tests_auto_regressions.py
+++ tests/tests_auto_regressions.py
@@ -9,6 +9,13 @@ class RegressionsTestCase(BaseTestCase):
         # that derivatives folder has no valid BIDS files
         self.assertEqual(0, len(all_derivative_files))
 
+    def test_get_metadata_no_params(self):
+        layout = ancpbids.BIDSLayout(DS005_DIR)
+        md = layout.get_metadata()
+        expected_md = {'RepetitionTime': 2.0, 'TaskName': 'mixed-gambles task',
+                       'SliceTiming': [0.0, 0.0571, 0.1143, 0.1714, 0.2286, 0.2857], 'run': 1}
+        self.assertEqual(md, expected_md)
+
 
 if __name__ == '__main__':
     unittest.main()
